3fd43fe6df1844f2089ef5221e65b30492e5aca1
izanelpro <izanlage161005@gmail.com>
2025-02-07 22:21:55 +0100

    ok

--- proyecto_fin_react/src/App.js
+++ proyecto_fin_react/src/App.js
@@ -1,39 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Inicio from './pages/Inicio/Inicio';
 import MyFit from './pages/MyFit/MyFit';
 import Comunidad from './pages/Comunidad/Comunidad';
 import Contacto from './pages/Contacto/Contacto';
-import Registrarse from './pages/Log/Logup/Registrarse';
 
-function Layout() {
-  const location = useLocation();
-
+function App() {
   return (
-    <>
-      {/* Ocultar NavBar en /registrarse */}
-      {location.pathname !== "/registrarse" && <NavBar />}
-
+    <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Inicio />} /> 
         <Route path="/myfit" element={<MyFit />} /> 
         <Route path="/comunidad" element={<Comunidad />} /> 
         <Route path="/contacto" element={<Contacto />} /> 
-        <Route path="/registrarse" element={<Registrarse />} /> 
       </Routes>
-
-      {/* Ocultar Footer en /registrarse si es necesario */}
-      {location.pathname !== "/registrarse" && <Footer />}
-    </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <Layout />
+      <Footer />
     </Router>
   );
 }
